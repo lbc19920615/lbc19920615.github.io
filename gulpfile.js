d831de4070785843e97627c4af8e00375d0559c4
lbc19920615 <1585638808@qq.com>
2021-03-02 20:09:53 +0800

    use index.twig

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,28 @@
-const { watch, task } = require('gulp');
+const gulp = require('gulp');
 const { exec } = require('child_process')
 
-task('default', async function watchMd () {
-  watch(['articles/*.md'], function(cb) {
+gulp.task('compile', function () {
+  'use strict';
+  var twig = require('gulp-twig');
+  return gulp.src('./index.twig')
+      .pipe(twig({
+          data: {
+            now: Date.now(),
+              // title: 'Gulp and Twig',
+              // benefits: [
+              //     'Fast',
+              //     'Flexible',
+              //     'Secure'
+              // ]
+          }
+      }))
+      .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', async function watchMd () {
+  gulp.watch(['component.js', 'plugin.js'], gulp.parallel(['compile']))
+
+  gulp.watch(['articles/*.md'], function(cb) {
     exec('npm run toc:articles', (err, stdout, stderr) => {
       if (err) {
         console.error(err)
