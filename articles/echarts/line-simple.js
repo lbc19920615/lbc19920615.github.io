9ca622019dffe3427ef16d848baf0d8669817ef8
lbc19920615 <1585638808@qq.com>
2021-03-24 15:01:38 +0800

    refresh echart

--- articles/echarts/line-simple.js
+++ articles/echarts/line-simple.js
@@ -1,28 +1,57 @@
-export function setLineSimple(document) {
-  var dom = document.getElementById("container");
-  var myChart = echarts.init(dom);
-  var app = {};
 
-  var option;
+ var myChart;
 
+function renderChart(document, option = {}) {
+    var dom = document.getElementById("container");
+    if (!myChart) {
+        myChart = echarts.init(dom);
+    } else {
+        myChart.clear()
+    }
 
+    var opt = Object.assign({}, option);
 
-  option = {
-      xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-      },
-      yAxis: {
-          type: 'value'
-      },
-      series: [{
-          data: [150, 230, 224, 218, 135, 147, 260],
-          type: 'line',
-          areaStyle: {}
-      }]
-  };
-
-  if (option && typeof option === 'object') {
-      myChart.setOption(option);
-  }
+    if (opt && typeof opt === 'object') {
+        myChart.setOption(opt);
+    }
 }
+
+
+export let map = new Map();
+
+export function setLineSimple(document) {
+    renderChart(document, {
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: [150, 230, 224, 218, 135, 147, 260],
+            type: 'line',
+            areaStyle: {}
+        }]
+    })
+}
+map.set('setLineSimple', setLineSimple);
+
+export function setLineSmooth(document) {
+    renderChart(document, {
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: [150, 230, 224, 218, 135, 147, 260],
+            type: 'line',
+            areaStyle: {},
+            smooth: true
+        }]
+    })
+}
+map.set('setLineSmooth', setLineSmooth);
