a58aa87bb1bac55a0a600fcef88522a78c9d0363
凌柏超 <1585638808@qq.com>
2021-04-03 09:01:37 +0800

    使用sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,27 @@ const { MyPlugin } = require('./src/plugins/my-webpack.plugin')
 
 module.exports = {
   entry: './src/index.js',
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.s[ac]ss$/i,
-  //       use: [
-  //         // Creates `style` nodes from JS strings
-  //         'style-loader',
-  //         // Translates CSS into CommonJS
-  //         'css-loader',
-  //         // Compiles Sass to CSS
-  //         {
-  //           loader: 'sass-loader',
-  //           options: {
-  //             // Prefer `dart-sass`
-  //             implementation: require('sass'),
-  //           },
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer `dart-sass`
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+    ],
+  },
 
   plugins: [
     new MyPlugin({
