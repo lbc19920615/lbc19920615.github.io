8b57d79743c76bc1ee7d93d70750167fff1ba5eb
凌柏超 <1585638808@qq.com>
2021-04-03 10:34:07 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,41 @@
-const gulp = require('gulp');
+const gulp = require('gulp')
 const { exec } = require('child_process')
 
-let folders = [
-  'articles/*.md',
-  'bug/*.md',
-  'flutter/*.md'
-]
+let folders = ['articles/*.md', 'bug/*.md', 'flutter/*.md']
 
 gulp.task('compile', function () {
-  'use strict';
-  var twig = require('gulp-twig');
-  return gulp.src('./index.twig')
-      .pipe(twig({
-          data: {
-            now: Date.now(),
-              // title: 'Gulp and Twig',
-              // benefits: [
-              //     'Fast',
-              //     'Flexible',
-              //     'Secure'
-              // ]
-          }
-      }))
-      .pipe(gulp.dest('./'));
-});
+  'use strict'
+  var twig = require('gulp-twig')
+  return gulp
+    .src('./index.twig')
+    .pipe(
+      twig({
+        data: {
+          now: Date.now(),
+          // title: 'Gulp and Twig',
+          // benefits: [
+          //     'Fast',
+          //     'Flexible',
+          //     'Secure'
+          // ]
+        },
+      })
+    )
+    .pipe(gulp.dest('./'))
+})
 
-gulp.task('default', async function watchMd () {
-  gulp.watch(['component.js', 'plugin.js', 'index.twig'], gulp.parallel(['compile']))
+gulp.task('default', async function watchMd() {
+  // gulp.watch(['component.js', 'plugin.js', 'index.twig'], gulp.parallel(['compile']))
 
-  
-  gulp.watch(folders, function(cb) {
+  gulp.watch(folders, function (cb) {
     exec('npm run toc:articles', (err, stdout, stderr) => {
       if (err) {
         console.error(err)
       }
       console.log(stdout)
       console.error(stderr)
-        // body omitted
-      cb();
+      // body omitted
+      cb()
     })
-  });
+  })
 })
