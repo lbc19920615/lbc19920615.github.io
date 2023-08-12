0de45e0dd345b8ee33ebc8e5aec981b6af2c4831
LBC-PC\lbc15 <1585638808@qq.com>
2023-08-12 20:49:04 +0800

    update

---
+++ rollup.config.mjs
@@ -0,0 +1,29 @@
+import { babel } from '@rollup/plugin-babel';
+
+function demoWatcherPlugin() {
+
+  return {
+      async buildEnd(arg) {
+          // console.log('ssss', arg);
+      },
+      // augmentChunkHash(chunkInfo) {
+      //   console.log(chunkInfo );
+      // }
+
+      generateBundle(options, bundle, isWrite) {
+        console.log(options, bundle, isWrite);
+      }
+  }
+}
+
+const config = {
+  input: 'src/main.js',
+  output: {
+    dir: 'assets',
+    format: 'es',
+    entryFileNames: 'webele_[hash].js' 
+  },
+  plugins: [babel({ babelHelpers: 'bundled' }), demoWatcherPlugin()]
+};
+
+export default config;
