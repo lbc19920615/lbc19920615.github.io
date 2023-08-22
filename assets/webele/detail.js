66d3f5a7bedc59d55d80b13769a607ea21bd331b
LBC-PC\lbc15 <1585638808@qq.com>
2023-08-22 14:40:56 +0800

    update

--- assets/webele/detail.js
+++ assets/webele/detail.js
@@ -54,54 +54,6 @@ let Text2 = defComponent({
     }
 })
 
-let SwiperNav1 = defComponent({
-    setup({getCompCtx, startWatch, args}) {         
-        let ele = document.createElement('div')
-        ele.classList.add('swiper-nav1');
-
-        let childrens = []
-        hc2(Column, {
-            init(ele)  {
-                hc2(Text, {args: ['swiper com 1'], attrs: {sindex: 0}}, ele)
-                hc2(Text, {args: ['swiper com 2'], attrs: {sindex: 1}}, ele)
-                hc2(Text, {args: ['swiper com 3'], attrs: {sindex: 2}}, ele)
-            },
-            end(ctx) {
-                ctx.ele.classList.add('dis-flex');
-                ele.style.width = 'max-content';
-                ele.style.transform = 'translateX(0)';
-                childrens = [...ctx.ele.children];
-            }
-        }, ele);
-
-        ele.addEventListener('click', (e) => {
-            // console.log(e.target, childrens);
-            if (childrens.includes(e.target)) {
-                let sindex = parseInt(e.target.getAttribute('sindex'));
-
-                ele.dispatchEvent(new CustomEvent('select-index', {
-                    detail: sindex
-                }))
-                console.log('ssssssssssss', sindex);
-            }
-        })
-
-    
-        function render(ele) {
-            // console.log(args[0]);
-            // let text = args[0]
-            // ele.textContent = text.__v_isRef ? text.value : text
-        }
-    
-        render(ele)
-    
-        startWatch(() => {
-            render(ele)
-        })
-
-        return ele
-    }
-})
 
 function genItems(num = 1000) {
     return new Array(num).fill(0).map((item,index) => 'item' + (index + 1))
@@ -147,46 +99,6 @@ export default function({Page}) {
     g.defc(Column().init(function (ele) {
 
         
-        let SwiperNav1Ctx = hc2(SwiperNav1, {args: []}, ele);
-        let SwiperNav1Ele = SwiperNav1Ctx.ele;
-        // console.log(SwiperNav1Ele);
-        SwiperNav1Ele.addEventListener('select-index', function(e) {
-            // console.dir( swCon.swiper)
-            if (swCon.swiper) {
-                if (swCon.swiper.realIndex === e.detail) {
-                    return
-                }
-                // console.dir( swCon.swiper.slideTo)
-                swCon.swiper.slideTo(e.detail)
-            }
-        })
-
-        let swCon = document.createElement('swiper-container');
-        swCon.id = 'swCon';
-        ele.appendChild(swCon);
-
-        hc2(ForEach, {
-            args: [{ max: 3 }], init: (ele, option) => {
-                // console.log('ele', ele)
-                let swSlide = document.createElement('swiper-slide')
-                swSlide.innerHTML = 'slide' + option.index;
-                swSlide.style.height = '180px';
-                option.appendChild(swSlide);
-           
-            },
-            ready() {
-                setTimeout(() => {
-                    swCon.swiper.on('setTranslate', (swiper, translate) =>{
-                        // virtualSize
-                        // console.log('translate', swiper.virtualSize, translate);
-                        SwiperNav1Ele.style.transform = `translateX(${translate / (swiper.virtualSize) * 100}%)`
-                    })
-                }, 30)
-            }
-        }, swCon);
-
-
-
         ; g.defc(Text2(vm.TextDetail).init(function (ele) {
         }), function (ctx) {
             ctx.done(ele)
